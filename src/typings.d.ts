8e841e241bf89ac2532c7c695c8f693d34e53d21
Eleven <jun.xi@mihoyo.com>
2022-09-28 20:02:20 +0800

    perf: add postcss

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -50,4 +50,19 @@ declare module '*.svg' {
   export default src;
 }
 
+declare module '*.module.css' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
+declare module '*.module.scss' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
+declare module '*.module.sass' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
 declare type BuildEnv = 'development' | 'test' | 'production';
